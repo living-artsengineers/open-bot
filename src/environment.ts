c929ee4a1f06bba6d0466392eaf0b8212247b348
Michael P <michael@mcmoo.org>
2022-08-07 12:51:49 -0400

    Adjust env.json reading for type consistency

--- src/environment.ts
+++ src/environment.ts
@@ -1,14 +1,16 @@
 // If you're missing env.json, download it from #open-bot-dev.
 import * as envConfig from "./env.json";
 
+const activeEnv: keyof typeof envConfig = "dev";
+
 interface Environment {
   guild: string;
   clientId: string;
   token: string;
-  name: "dev" | "prod";
+  name: keyof typeof envConfig;
 }
 
 export default {
-  ...envConfig[envConfig.env],
-  name: envConfig.env,
+  ...envConfig[activeEnv],
+  name: activeEnv,
 } as Environment;
